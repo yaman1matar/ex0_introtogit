13dd02d0974e7d1f4d8af31843656adfb7ff4ae9
yaman1matar <129399274+yaman1matar@users.noreply.github.com>
2023-04-16 21:57:59 +0300

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int course_number[6] = {4, 2, 1, 4, 3, 2};
 int main() {
+	int course_number[6] = {4, 2, 1, 4, 3, 2};
 	for(int i = 6; i > 0; i--) {
 		printf("%d", course_number[i]);
 	}
